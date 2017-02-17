1d0e851dbad7d693989177beceff328f2f2b7f48
xiening103 <1031747891@qq.com>
2017-02-17 10:19:17 +0800

    add Popup

--- src/js/components/common/popup/index.js
+++ src/js/components/common/popup/index.js
@@ -1,4 +1,42 @@
 /**
  * Created by xiening on 2017/2/16.
  */
+import React from 'react';
+import Popup from '../../h-ui/Popup';
 
+const code = `
+示例代码
+`;
+
+class DemoPopup extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.refs.code.innerText = code;
+        var _t = this;
+        hljs.highlightBlock(_t.refs.code);
+        // setTimeout(function () {
+        //   // hljs.initHighlighting();
+        // }, 200)
+    }
+
+    render() {
+        return (
+            <div className="Popup">
+                <p style={{marginBottom:'30px'}}>popup切换(Popup)组件：</p>
+                {/*<Popup></Popup>*/}
+                <Popup> </Popup>
+                <p style={{marginTop:'30px'}}>示例代码：</p>
+                <div>
+                     <pre>
+                        <code ref="code" className="js"></code>
+                     </pre>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DemoPopup;
--- src/js/components/h-ui/Popup/index.js
+++ src/js/components/h-ui/Popup/index.js
@@ -1,3 +1,30 @@
 /**
  * Created by xiening on 2017/2/16.
  */
+import React from 'react';
+import './style.scss';
+
+class Popup extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                <div className = "m-mask"></div>
+                <div className = "m-dialog">
+                    <div className="md-dialog">
+                        <div className="md-dialog-title">
+                            <h4>{title}</h4>
+                            <span className="btn">
+                                <i className="iconfont">&time</i>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Popup;
