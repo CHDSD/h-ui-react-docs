17b6ae18db5033d7ff3539226c7594f037ee6204
xiening103 <1031747891@qq.com>
2017-02-20 18:07:36 +0800

    Popup add function

--- src/js/components/common/popup/index.js
+++ src/js/components/common/popup/index.js
@@ -23,11 +23,27 @@ class DemoPopup extends React.Component {
     }
 
     render() {
+        var popupData = {
+            title:'提示框标题',
+            content:'提示信息内容',
+            time: 3000
+            //callBack:function(b){
+            //    if(b){
+            //        console.log("点击了确定按钮"+b);
+            //    }else{
+            //        console.log("点击了取消按钮"+b);
+            //    }
+            //}
+        }
         return (
             <div className="Popup">
                 <p style={{marginBottom:'30px'}}>popup切换(Popup)组件：</p>
                 {/*<Popup></Popup>*/}
-                <Popup> </Popup>
+                <Popup {...popupData}>
+                    <a href="javascript:;" className="alertPopup">alert</a>
+                    <a href="javascript:;" className="alertPopup" style={{marginTop:'15px'}} >带回调函数的popup</a>
+                    <a href="javascript:;" className="alertPopup" style={{marginTop:'15px'}} >3秒关闭</a>
+                </Popup>
                 <p style={{marginTop:'30px'}}>示例代码：</p>
                 <div>
                      <pre>
