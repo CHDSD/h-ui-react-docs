a98487b99ba1e0bb065adf12fb41219ec823668e
xiening103 <1031747891@qq.com>
2017-02-21 14:35:40 +0800

    Popup modify bug

--- src/js/components/common/popup/index.js
+++ src/js/components/common/popup/index.js
@@ -24,16 +24,27 @@ class DemoPopup extends React.Component {
 
     render() {
         var popupData = {
-            title:'提示框标题',
-            content:'提示信息内容',
+            title:'提示信息标题1',
+            content:'提示信息内容1'
+
+        }
+        var popupData2 = {
+            title:'提示信息标题2',
+            content:'提示信息内容2',
+            cancelText:'否',
+            confirmText:'是',
+            callBack:function(b){
+                if(b){
+                    console.log("点击了"+this.confirmText+"按钮"+b);
+                }else{
+                    console.log("点击了"+this.cancelText+"按钮"+b);
+                }
+            }
+        }
+        var popupData3 = {
+            title:'提示信息标题3',
+            content:'提示信息内容3',
             time: 3000
-            //callBack:function(b){
-            //    if(b){
-            //        console.log("点击了确定按钮"+b);
-            //    }else{
-            //        console.log("点击了取消按钮"+b);
-            //    }
-            //}
         }
         return (
             <div className="Popup">
@@ -41,7 +52,11 @@ class DemoPopup extends React.Component {
                 {/*<Popup></Popup>*/}
                 <Popup {...popupData}>
                     <a href="javascript:;" className="alertPopup">alert</a>
+                </Popup>
+                <Popup {...popupData2}>
                     <a href="javascript:;" className="alertPopup" style={{marginTop:'15px'}} >带回调函数的popup</a>
+                </Popup>
+                <Popup {...popupData3}>
                     <a href="javascript:;" className="alertPopup" style={{marginTop:'15px'}} >3秒关闭</a>
                 </Popup>
                 <p style={{marginTop:'30px'}}>示例代码：</p>
--- src/js/components/h-ui/Popup/index.js
+++ src/js/components/h-ui/Popup/index.js
@@ -5,42 +5,45 @@ import React from 'react';
 import './style.scss';
 
 class Popup extends React.Component {
+
     constructor(props) {
         super(props);
-        this.click=this.click.bind(this);
-        this.closePopup=this.closePopup.bind(this);
-        this.cancel=this.cancel.bind(this);
-        this.confirm=this.confirm.bind(this);
-        this.autoClose=this.autoClose.bind(this);
+        this.click = this.click.bind(this);
+        this.closePopup = this.closePopup.bind(this);
+        this.cancel = this.cancel.bind(this);
+        this.confirm = this.confirm.bind(this);
+        this.autoClose = this.autoClose.bind(this);
+        var timestamp = new Date().getTime();
+        this.popupBox = "popupBox"+timestamp;
+        this.popupMask = "popupMask"+timestamp;
     }
-
     click(event) {
-        document.getElementById('popupMask').style.display='block';
-        document.getElementById('popupBox').style.display='block';
+        var popupBox = document.getElementById(this.popupBox+""),
+            popupMask = document.getElementById(this.popupMask+""),
+            pHeight = document.body.clientHeight,
+            pWidth = document.body.clientWidth,
+            pBHeight = popupBox.offsetHeight,
+            pBWidth = popupBox.offsetWidth;
 
-        var popupBox=document.getElementById('popupBox');
-        var pHeight=document.body.clientHeight;
-        var pWidth=document.body.clientWidth;
-        var pBHeight=popupBox.offsetHeight;
-        var pBWidth=popupBox.offsetWidth;
+        popupBox.style.display = 'block';
+        popupMask.style.display = 'block';
 
-        popupBox.style.top=pHeight/2-pBHeight/2 +'px';
-        popupBox.style.left=pWidth/2-pBWidth/2 +'px';
+        popupBox.style.top = pHeight/2-pBHeight/2 +'px';
+        popupBox.style.left = pWidth/2-pBWidth/2 +'px';
 
-        if(this.props.callBack !=undefined){
-            document.getElementById('popupBoxFoot').style.display='block';
-        }else{
-            //document.getElementById('popupBoxFoot').style.display='none';
+        if(this.props.callBack != undefined){
+            document.getElementById('popupBoxFoot').style.display = 'block';
         }
-        if(this.props.time !=null){
-            //console.debug(this.props.time);
+        if(this.props.time != null){
             this.autoClose(event);
         }
-
     }
     closePopup(event){
-        document.getElementById('popupMask').style.display='none';
-        document.getElementById('popupBox').style.display='none';
+        var popupBox = document.getElementById(this.popupBox+""),
+            popupMask = document.getElementById(this.popupMask+"");
+
+        popupMask.style.display = 'none';
+        popupBox.style.display = 'none';
     }
     cancel(event){
         this.closePopup(event);
@@ -51,42 +54,39 @@ class Popup extends React.Component {
         this.props.callBack(true);
     }
     autoClose(event){
-        //console.debug(this.props.time);
         if(this.props.time != null){
           setTimeout(function(){
                 this.closePopup(event);
             }.bind(this),3000);
         }
     }
-
     getFoot() {
         if (this.props.callBack != undefined) {
             return (
                 <div id="popupBoxFoot" className="popupBoxFoot">
-                <a onClick={this.cancel} style={{color:'#666',display: 'inline-block',padding: '5px 10px',background:'#e6e6e6',marginRight:'25px'}}>取消</a>
-                <a onClick={this.confirm} style={{color:'#fff',display: 'inline-block',padding: '5px 10px',background:'#0e90d2'}}>确定</a>
+                <a onClick={this.cancel} style={{color:'#666',display: 'inline-block',padding: '5px 10px',background:'#e6e6e6',marginRight:'25px'}}>{this.props.cancelText}</a>
+                <a onClick={this.confirm} style={{color:'#fff',display: 'inline-block',padding: '5px 10px',background:'#0e90d2'}}>{this.props.confirmText}</a>
             </div>
             );
         } else {
             return null;
         }
     }
-
     render(){
         let popupBoxFoot = this.getFoot();
         return(
             <div className="Popup">
-                <div id="popupMask" className="popupMask"></div>
+                <div id={this.popupMask} className="popupMask"></div>
                 {React.Children.map(this.props.children,(element) => {
                     return(
                         <div onClick={this.click}>{element}</div>
                     );
                 })
                 }
-                <div id="popupBox" className="popupBox" >
-                    <h1 className="popupBoxTitle">系统提示</h1>
+                <div id={this.popupBox} className="popupBox" >
+                    <h1 className="popupBoxTitle">{this.props.title}</h1>
                     <a href="javascript:;" className="popupBoxCloseBtn" onClick={this.closePopup}><i className="iconClose"></i></a>
-                    <div className="popupBoxBody">系统提示信息</div>
+                    <div className="popupBoxBody">{this.props.content}</div>
                     {popupBoxFoot}
                 </div>
             </div>
